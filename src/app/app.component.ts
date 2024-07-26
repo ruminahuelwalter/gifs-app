35160cc8f135d4b5ed8280f36e3e4ca99f7f6d4d
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-07-26 11:10:44 -0300

    app.config provideHttpClient(withFecth()) agregado, por problemas al inyectar HttpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
@@ -6,7 +6,7 @@ import { GifsModule } from './gifs/gifs.module';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SharedModule, GifsModule],
+  imports: [RouterOutlet, SharedModule, GifsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
-    constructor() { }
-    
+    constructor( private http: HttpClient ) { }
+
     private _tagsHistory: string[] = [];
-    
+    private apiKey: string = 'zOkEnADY5KW8UTn63MaJr1M6HE3CvOyS';
+    private serviceUrl = 'http://api.giphy.com/v1/gifs';
+
     get tagsHistory() {
         return [ ...this._tagsHistory]
     }
@@ -18,14 +21,23 @@ export class GifsService {
         this._tagsHistory.unshift( tag );
         this._tagsHistory = this.tagsHistory.splice(0,10);
     }
+
+
     searchTag( tag: string ):void {
         if (tag.length === 0 ) return
         this.organizeHistory( tag );
 
-        //this._tagsHistory.unshift( tag );
-        console.log(this.tagsHistory);
+        const params = new HttpParams()
+        .set('api_key', this.apiKey)
+        .set('limit', '10')
+        .set('q', tag)
+
+        this.http.get(`${ this.serviceUrl }/search?`,{ params })
+        .subscribe( resp => {
+            console.log( resp );
+        });
+        
+
     }
-
-
 
 }
