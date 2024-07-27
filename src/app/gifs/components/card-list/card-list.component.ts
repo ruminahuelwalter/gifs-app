5d8a538db1187ada0687f30006a97a78e8a6084e
ruminahuelwalter <ruminahuelwalter@gmail.com>
2024-07-27 00:04:14 -0300

    card.component image-loader

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Gif } from '../../Interfaces/gifs.interface';
+import { GifsCardComponent } from "../card/card.component";
 
 @Component({
   selector: 'gifs-card-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GifsCardComponent],
   templateUrl: './card-list.component.html',
   styleUrl: './card-list.component.css'
 })
@@ -13,5 +14,4 @@ export class CardListComponent {
 
   @Input()
   public gifs: Gif[] = []
-
 }
---
+++ src/app/gifs/components/card/card.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../Interfaces/gifs.interface';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'gifs-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.css'
+})
+export class GifsCardComponent implements OnInit {
+  @Input()
+  public gif!: Gif;
+
+  ngOnInit(): void {
+    if (!this.gif) throw new Error('Gifs property is required');
+  }
+}
